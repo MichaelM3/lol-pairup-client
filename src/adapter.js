fbf24718c3c2653471ad331442ca6e2d9b22a4c6
MichaelM3 <michael.muniz94@gmail.com>
2019-04-02 13:16:03 -0400

    fixed all adapter issues

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,7 @@
-export const ENDPOINT_URL = 'lol-pairup-backend.herokuapp.com'
+export const ENDPOINT_URL = 'https://lol-pairup-backend.herokuapp.com'
 
-// export const ENDPOINT_URL = 'localhost3000.com'
+export const WS_URL = 'wss://lol-pairup-backend.herokuapp.com/cable'
+
+// export const ENDPOINT_URL = 'http://localhost:3000'
+
+// export const WS_URL = 'ws://localhost:3000/cable'
