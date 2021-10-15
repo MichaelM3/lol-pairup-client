0f386cf32931ee0ee17ada2faad9048ebbf6e9a5
michaelm3 <michael.muniz94@gmail.com>
2021-10-15 12:37:28 -0400

    updated packages

--- src/adapter.js
+++ src/adapter.js
@@ -1,7 +1,7 @@
-export const ENDPOINT_URL = 'https://lol-pairup-backend.herokuapp.com'
+// export const ENDPOINT_URL = 'https://lol-pairup-backend.herokuapp.com'
 
-export const WS_URL = 'wss://lol-pairup-backend.herokuapp.com/cable'
+// export const WS_URL = 'wss://lol-pairup-backend.herokuapp.com/cable'
 
-// export const ENDPOINT_URL = 'http://localhost:3000'
+export const ENDPOINT_URL = 'http://localhost:3000'
 
-// export const WS_URL = 'ws://localhost:3000/cable'
+export const WS_URL = 'ws://localhost:3000/cable'
